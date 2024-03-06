db20efef1219ff1f56cb6f1a271077a1784e4ca2
futurebound <mitchellm1618@gmail.com>
2024-03-06 10:56:40 -0800

    feat: completed exercise 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	if (str.length <= 1) return str
 
+	let reversed = ""
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversed += str.at(i)
+	}
+	return reversed
 };
 
 // Do not edit below this line
