2e3c1c947fbd8c1da931be0abf10c78f6a8af91b
futurebound <mitchellm1618@gmail.com>
2024-03-07 14:51:56 -0800

    feat: completed exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+	if (num1 < 0 || num2 < 0 || 
+			typeof num1 !== "number" || typeof num2 !== "number") 
+		return "ERROR";
 
+	let sum = 0;
+	const start = num1 < num2 ? num1 : num2;
+	const end = num2 > num1 ? num2 : num1;
+
+	for (let i = start; i <= end; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
