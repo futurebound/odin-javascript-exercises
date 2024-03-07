29800db415137a44816649b5471b4fe178e9c716
futurebound <mitchellm1618@gmail.com>
2024-03-07 14:39:28 -0800

    feat: completed exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+	for (let i = 0; i < arr.length; i++) {
+		for (const item of items) {
+			if (arr[i] === item) {
+				arr.splice(i, 1);
+				i--;
+			}
+		}
+	}
 
+	return arr;
 };
 
 // Do not edit below this line
