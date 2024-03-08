46c19c906398ad0aa55ba7ee80d81f047febcf10
futurebound <mitchellm1618@gmail.com>
2024-03-07 16:28:09 -0800

    feat: completed exercise 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // °C = (°F - 32) × 5/9
+  const celcius = (temp - 32) * (5.0 / 9);
+  return parseFloat(celcius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // °F = (°C × 9/5) + 32 
+  const fahrenheit = (temp * (9.0 / 5)) + 32;
+  return parseFloat(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
