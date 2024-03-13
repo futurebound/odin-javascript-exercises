67448c03b5b934e812a874a8b92126d12b7dee54
futurebound <mitchellm1618@gmail.com>
2024-03-13 16:59:52 -0700

    feat: completed exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+// likely suboptimal, but only by a factor of 1/2 * n
+//  could also check alphanumeric in the for-loop, and
+//  then increment/decrement indices as needed 
+// loose definition of palindrome here so not worrying about it
 
+const palindromes = function (str) {
+  str = processString(str)
+
+  for (let i = 0; i < str.length / 2; i++) {
+    let a = str.charAt(i)
+    let b = str.charAt(str.length - (1 + i))
+    
+    if (a !== b) return false
+  }
+
+  return true
 };
+
+const processString = function (str) {
+  let processed = ""
+  const strRegex = new RegExp(/[^a-zA-Z0-9]/)
+
+  str = str.toLowerCase()
+  for (let i = 0; i < str.length; i++) {
+    if (!strRegex.test(str.charAt(i))) { // false -> no problems
+      processed += str.charAt(i)
+    }
+  }
+  return processed
+}
 
 // Do not edit below this line
 module.exports = palindromes;
