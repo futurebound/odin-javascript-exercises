a5fbc5fda464ea66eccec8cc1ed1647bcaf4dcc7
futurebound <mitchellm1618@gmail.com>
2024-03-13 17:25:09 -0700

    feat: completed exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldest = people
+      .sort((a, b) => {
+        console.log(`a.death: ${a.yearOfDeath} b.death: ${b.yearOfDeath}`)
+        const aAge = a.yearOfDeath 
+            ? a.yearOfDeath - a.yearOfBirth 
+            : new Date().getFullYear() - a.yearOfBirth
+        const bAge = b.yearOfDeath 
+            ? b.yearOfDeath - b.yearOfBirth 
+            : new Date().getFullYear() - b.yearOfBirth
+        return bAge - aAge
+      })
 
+  // sorted so oldest age is first
+  return oldest[0]
 };
 
 // Do not edit below this line
